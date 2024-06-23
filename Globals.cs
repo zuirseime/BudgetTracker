da6c3731a142efbbf339c72f65c88dbbb1e81b93
zuirseime <vovakibets88@gmail.com>
2024-06-23 12:25:05 +0300

    Update globlas

--- Globals.cs
+++ Globals.cs
@@ -4,4 +4,14 @@ public static class Globals
 {
     public const string AdminRole = "Admin";
     public const string UserRole = "User";
+    public const string ImageFolder = "wwwroot/content/images";
+    public const string DefaultPicture = "ImagePlaceholder.png";
+
+    public static string GetUserPic(string image, bool truncate = true) {
+        if (truncate) {
+            int firstSlash = ImageFolder.IndexOf('/');
+            return $"{ImageFolder[firstSlash..]}/{image}";
+        }
+        else return $"{ImageFolder}/{image}";
+    }
 }
